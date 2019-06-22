41c8896be4c88640be66bc6a250dfd17d4152f2e
dirizarry3 <danielssimplerobotics@gmail.com>
2019-06-21 21:42:34 -0400

    Update kalman_filter.py

--- kalman_filter.py
+++ kalman_filter.py
@@ -5,14 +5,27 @@ class KalmanFilter:
     #update rate is in hertz
     def __init__(dim=2, updateRate=5):
     
-        self.timeDelta = 1.0/updateRate
+        self.dt = 1.0/updateRate
     
         if dim == 2:
-            # stateVector = [x, y, vx, vy, ax, ay]
-            self.stateVector = np.array([[0, 0, 0, 0, 0, 0]])
+            # stateVector = [x, y, vx, vy]
+            self.stateVector = np.array([[0, 0, 0, 0]])
+            
+            # controlVector(u) = [ax, ay]
+            self.controlVector = np.array([0, 0])
+            
+            # stateTransitionMatrix(A)
+            self.A = np.array([[1, 0, self.dt],[0, 1, , 0, self.dt],[0,0,1,0],[0,0,0,1]])
+            
+            #controlTransitionMat(B)
+            self.B = np.array([[0.5*(self.dt**2), 0],[0, 0.5*(self.dt**2)],[self.dt,0],[0,self.dt]])
+        
         elif dim == 3:
-            # stateVector = [x, y, z, vx, vy, vz, ax, ay, az]
-            self.stateVector = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0]])
+            # stateVector (x)= [x, y, z, vx, vy, vz]
+            self.stateVector = np.array([[0, 0, 0, 0, 0, 0]])
+            
+            # controlVector (u) = [ax, ay, az]
+            self.controlVector = np.array([0, 0, 0])
         else:
             print("incorrect dimensions for filter")
         return
@@ -22,5 +35,5 @@ class KalmanFilter:
         return
         
     def update():
-    
+        #objective: xk = A*xk-1 + b*uk + q(noise)
         return
